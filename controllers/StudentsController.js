b8a3e144506db9de55fb39d96f3d145ec8e1b108
Don Schemmel <dons500@gmail.com>
2015-09-29 16:43:15 -0700

    Make sure up to date

--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -2,10 +2,9 @@ signIn.controller('StudentsCtrl', function StudentsCtrl($scope, StudentsFactory)
   $scope.students = StudentsFactory.students;
   $scope.signIn = function(student) {
     student.signedIn = true;
-  }
+  };
   $scope.signOut = function(student) {
     student.signedIn = false;
-  }
-
+  };
   $scope.StudentsFactory = StudentsFactory;
 });
--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,7 +1,11 @@
 signIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
-  factory.students = [{ name: "Don Schemmel", signedIn: false },
-                      { name: "Mike Scott", signedIn: false}];
+
+  factory.students = [{ name: "Don Schemmel", signedIn: true },
+    { name: "Mike Scott", signedIn: false },
+    { name: "Ann Johnson", signedIn: false },
+    { name: "Paul Jones", signedIn: false }
+    ];
 
   factory.addStudent = function() {
     factory.students.push({ name: factory.studentName, signedIn: false });
