ad5e8f83a5394c0f97f4d6350e3c0a0883d1b8ec
Don Schemmel <dons500@gmail.com>
2015-09-29 19:54:11 -0700

    Names of students show

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,11 +1,12 @@
 signIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
 
-  factory.students = [{ name: "Don Schemmel", signedIn: true },
+  factory.students = [
+    { name: "Don Schemmel", signedIn: false },
     { name: "Mike Scott", signedIn: false },
     { name: "Ann Johnson", signedIn: false },
-    { name: "Paul Jones", signedIn: false }
-    ];
+    { name: "Paul Jones", signedIn: false },
+  ];
 
   factory.addStudent = function() {
     factory.students.push({ name: factory.studentName, signedIn: false });
