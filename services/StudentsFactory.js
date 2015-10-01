2d0df57c7caf2334cc1ef275fdd6e066eac852fc
Don Schemmel <dons500@gmail.com>
2015-10-01 10:10:15 -0700

    Working somewhat, need code cleaned up and better functionality

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -2,7 +2,7 @@ signIn.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
 
   factory.students = [
-    { name: "Don Schemmel", signedIn: false },
+    { name: "Don Schemmel", signedIn: true },
     { name: "Mike Scott", signedIn: false },
     { name: "Ann Johnson", signedIn: false },
     { name: "Paul Jones", signedIn: false },
